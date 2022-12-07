e68bb7d5825443e16ee1f443aff7e3907b928a47
jeremy-deurvillier <88447272+jeremy-deurvillier@users.noreply.github.com>
2022-12-07 13:34:16 +0100

    Modification des infos du package.json. Mise en place du fichier CSS.

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
+import css from './login.module.css';
+
 export default function Login() {
   return (
     <div>
       <h1>Connexion</h1>
       <form action="#" method="POST">
-        <div className="form-content">
-          <div className="loginBlock">
+        <div className={css.formContent}>
+          <div className={css.loginBlock}>
             <label htmlFor="login">Login :</label>
             <input type="text" id="login" name="login" />
           </div>
-          <div className="passwordBlock">
+          <div className={css.passwordBlock}>
             <label htmlFor="password">Mot de passe :</label>
             <input type="password" id="password" name="password" />
           </div>
-          <div className="submitBlock">
+          <div className={css.submitBlock}>
             <button type="button">Connexion</button>
           </div>
         </div>
