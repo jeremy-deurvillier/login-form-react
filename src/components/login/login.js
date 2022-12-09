8ec94f97dafd1cf79c1f0feefe535dd4b5a3a7d4
jeremy-deurvillier <88447272+jeremy-deurvillier@users.noreply.github.com>
2022-12-09 11:58:57 +0100

    Mise en place finale de l'UI.

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -3,10 +3,6 @@
  *
  * TODO :
  * - Ajout d'un fonction permettant d'appeler le callback utilisateur pour se connecter lors du clic sur "connexion".
- * - Ajout d'un bouton "retour" (vers l'accueil ? autre ?)
- * - Ajout d'un toggle "Remember me"
- * - Ajout d'un bouton voir/cacher le mot de passe.
- * - Ajout des liens "Mot de passe oublié" et "S'inscrire"
  * - Implémentation de composants contrôlés sur les inputs.
  */
 
@@ -17,6 +13,9 @@ import css from './login.module.css';
 export default function Login() {
   return (
     <div className={css.loginForm}>
+      <a href="#links.back" className={css.btnBack}>
+        {'< Retour'}
+      </a>
       <h1>Connexion</h1>
       <div className={css.formContent}>
         <form action="#" method="POST">
@@ -27,9 +26,31 @@ export default function Login() {
           <div className={css.passwordBlock}>
             <label htmlFor="password">Mot de passe :</label>
             <input type="password" id="password" name="password" />
+            <button type="button" className={css.btnVisiblePassword}>
+              Voir
+            </button>
+            <div className={css.forgotPasswordBlock}>
+              <a href="#links.forgot" className={css.forgotLink}>
+                Mot de passe oublié
+              </a>
+            </div>
+          </div>
+          <div className={css.rememberMeBlock}>
+            <input type="checkbox" id="rememberMe" name="rememberMe" />
+            <span className={css.customCheckBox}></span>
+            <label htmlFor="rememberMe" tabIndex="0">
+              Rester connecté
+            </label>
           </div>
           <div className={css.submitBlock}>
-            <button type="button">Connexion</button>
+            <button type="button" className={css.submit}>
+              Me connecter
+            </button>
+          </div>
+          <div className={css.signInBlock}>
+            <a href="#links.signin" className={css.signInLink}>
+              M'inscrire
+            </a>
           </div>
         </form>
       </div>
